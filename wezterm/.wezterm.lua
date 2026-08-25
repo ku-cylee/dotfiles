ef68f6bcd6540c68fa5274aa5ce071e28586eeb7
Chanyoung Lee <btstar1998@gmail.com>
2026-08-25 15:53:26 +0900

    Add wezterm config

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,23 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.color_scheme = 'Material Darker (base16)'
+config.colors = {
+  cursor_border = wezterm.color.get_builtin_schemes()[config.color_scheme].background,
+}
+
+config.font = wezterm.font_with_fallback {
+  { family = 'Geist Mono', harfbuzz_features = { 'ss11=1' } },
+  'Sarasa Term K',
+}
+
+config.cursor_blink_rate = 0
+config.default_cursor_style = 'SteadyBar'
+
+config.keys = {
+  { key = 'phys:Minus', mods = 'CTRL|SHIFT', action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' } },
+  { key = 'phys:Backslash', mods = 'CTRL|SHIFT', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' } },
+  { key = 'q', mods = 'CTRL|SHIFT', action = wezterm.action.CloseCurrentPane { confirm = true } },
+}
+
+return config
